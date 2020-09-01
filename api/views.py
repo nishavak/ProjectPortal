59610708236841b859c6e8b3640813df00135d17
Nishavak <44199073+nishavak@users.noreply.github.com>
2020-09-01 16:53:10 +0530

    email verification added

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.core.mail import send_mail
+from django.contrib.auth import get_user_model
+from .tokens import account_activation_token
+from django.utils.encoding import force_bytes, force_text
+from django.template.loader import render_to_string
+from django.contrib.sites import get_current_site
 from django.conf import settings
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import Group
@@ -1485,13 +1492,38 @@ def signUp(request):
             "roll_number": roll_number,
             # "photo": None,
             "is_staff": False,
-            "is_active": True
+            "is_active": False
         }
         serializer = StudentSerializer(data=data)
         if serializer.is_valid():
-            serializer.save()
+            student = serializer.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your account.'
+            message = render_to_string('email_template.html', {
+                'user': student,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(student.pk)),
+                'token': account_activation_token.make_token(student),
+            })
+            to_email = email
+            send_mail(mail_subject, message, 'youremail', [to_email])
             return Response(status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def activate(request, uidb64, token):
+    User = get_user_model()
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
 
 
 @api_view(['POST'])
